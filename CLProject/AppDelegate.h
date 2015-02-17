093889a16c48c0410178b01909590bfd9958d5c1
Vasiliy Zhuk <vasyares@gmail.com>
2015-02-17 12:24:26 +0300

    Initial Commit

---
+++ CLProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CLProject
+//
+//  Created by Vasiliy Zhuk on 2/17/15.
+//  Copyright (c) 2015 Vasiliy Zhuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CLProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CLProject
+//
+//  Created by Vasiliy Zhuk on 2/17/15.
+//  Copyright (c) 2015 Vasiliy Zhuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
